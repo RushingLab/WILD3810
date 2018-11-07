fa96cea965cf9cce2cc61395bb699096e9b4d7ac
Clark Rushing <crushing05@gmail.com>
2018-11-07 10:08:20 -0700

    Update schedule and syllabus

--- inst/doc/schedule.R
+++ inst/doc/schedule.R
@@ -7,13 +7,25 @@ knitr::opts_chunk$set(
 
 ## ----schedule------------------------------------------------------------
 library(kableExtra)
-schedule <- data.frame(Week = seq(1:15),
-                       '1' = c("[Intro to WILD3810](lecture1.html)", rep(NA, 14)),
-                       '2' = c("[Abundance](lecture1.html)", rep(NA, 14)),
-                       Lab = c("Introduction to R", rep(NA, 14)))
+schedule <- data.frame(Week = c("1 (Jan. 7 & 9)", "2 (Jan. 14 & 16)", "3 (Jan. 21 & 23)", "4 (Jan. 28 & 30)",
+                                "5 (Feb. 4 & 6)", "6 (Feb. 11 & 13)", "7 (Feb. 18 & 20)", "8 (Feb. 25 & 27)",
+                                "9 (Mar. 4 & 6)", "10 (Mar. 11 & 13)", "11 (Mar. 18 & 20)", "12 (Mar. 25 & 27)",
+                                "13 (Apr. 1 & 3)", "14 (Apr. 8 & 10)", "15 (Apr. 15 & 17)", "16 (Apr. 22 & 24)"),
+                       M = c("[Intro to WILD3810](lecture1.html)", NA, "MLK Holiday - no class", NA,
+                             NA, NA, "President's Day - no class", NA,
+                             NA, "Spring break - no class", NA, NA,
+                             NA, NA, NA, NA),
+                       W = c("[Abundance](lecture1.html)", NA, NA, NA,
+                             NA, NA, NA, NA,
+                             NA, "Spring break - no class", NA, NA,
+                             NA, NA, NA, "No class"),
+                       Lab = c("Introduction to R", NA, "MLK Holiday - no lab", NA,
+                             NA, NA, "President's Day - no lab", NA,
+                             NA, "Spring break - no lab", NA, NA,
+                             NA, NA, NA, NA))
 
 schedule %>%
-  kable("html", col.names = c("Week", "1", "2", "Lab"), align = 'c') %>%
+  kable("html", col.names = c("Week (dates)", "M", "W", "Lab"), align = 'c') %>%
   kable_styling(bootstrap_options = c("striped", "hover", "condensed", "responsive"), full_width = TRUE) %>%
   add_header_above(c("", "Lecture" = 2, ""))
 
